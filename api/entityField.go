87ec3e95206102b6ecfeffd40682d4d25350ddc6
Ales <dev.ales.k@gmail.com>
2017-11-28 18:07:10 +0100

    added todos

--- api/entityField.go
+++ api/entityField.go
@@ -1,31 +1,35 @@
 package api
 
-// todo: add ALS rules: read, write, ...
-// todo: grouping (productName, productPrice, ...)
 type Field struct {
-	Label      string `json:"label"`
-	Name       string `json:"name"`
-	IsRequired bool   `json:"isRequired"`
-	Hidden     bool   `json:"hidden"`
+	Label    string `json:"label"`
+	Name     string `json:"name"`
+
+	Required bool   `json:"required"`
+	Hidden   bool   `json:"hidden"`
+	Multiple bool   `json:"multiple"`
+	NoIndex  bool   `json:"noIndex"`
 
 	Rules Rules `json:"rules"`
 
+	// Entity -> could be type Key; Embedded Entity -> type Embedded
+	// todo: type could be interface for it to be simple to embed entity or embedded entity; could also remove Entity property
 	Type Type `json:"type"`
 
+	// todo: add some group-like/embedded-entity field implementation? gob data encoding/decoding?
 	Entity *Entity `json:"-"`      // if set, value should be encoded entity key
-	Lookup bool    `json:"lookup"` // if true, looks up entity value on output
+	Lookup bool    `json:"lookup"` // if true, looks up entity value on output; todo: this could be query specific and not global setting
 
+	// todo: single Value property of type interface -> checks types on init; could have struct types for ValueFunc, ContextFunc, ...
 	DefaultValue interface{}                   `json:"defaultValue"`
 	ValueFunc    func() interface{}            `json:"-"`
 	ContextFunc  func(ctx Context) interface{} `json:"-"`
 
-	Multiple      bool                                                            `json:"multiple"`
-	NoIndex       bool                                                            `json:"noIndex"`
 	ValidateRgx   string                                                          `json:"validate"`
 	TransformFunc func(ctx *ValueContext, value interface{}) (interface{}, error) `json:"-"`
 	Validator     func(value interface{}) bool                                    `json:"-"`
 
 	//GroupEntity GroupEntity `json:"groupEntity"`   // if defined, value stored as an separate entity; in field stored key
+	// todo: I'd like to remove this; move evertything away from this kind?
 	Meta Meta `json:"meta"` // used for automatic admin html template creation
 
 	fieldFunc []func(ctx *ValueContext, v interface{}) (interface{}, error) `json:"-"`
